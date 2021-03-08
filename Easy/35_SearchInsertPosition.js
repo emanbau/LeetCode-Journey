2fe46ccbbd27b2b51dbb978029ef57d744e179f1
Eman Bau <emanbautista410@gmail.com>
2021-03-08 18:41:59 -0500

    28 & 35 Solutions

---
+++ Easy/35_SearchInsertPosition.js
@@ -0,0 +1,8 @@
+var searchInsert = function(nums, target) {
+    if (target < nums[0]) { return 0 }
+    if (target > nums[nums.length - 1]) { return nums.length }    
+    for (let i = 0; i < nums.length; i++) {
+        if (target > nums[i] && target < nums[i + 1]) { return i + 1 }
+        if (target == nums[i]) { return i }
+    }
+};
